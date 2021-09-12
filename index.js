2db2927b8ae1301bb9a0006f8be00dd3137cb235
Kuba 8515 <jakub.gazdziak@gmail.com>
2021-09-12 21:03:22 +0200

    Add files to folder

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 import fs from 'node:fs';
 import path, { dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
+import cheerio from 'cheerio';
 import fetch from 'node-fetch';
 
+// Create "memes" folder if it doesn't exist.
 const currentDir = dirname(fileURLToPath(import.meta.url));
-
-const response = await fetch(
-  'https://memegen-link-examples-upleveled.netlify.app/',
-);
-const body = await response.text();
-console.log(body);
-
 fs.mkdir(path.join(currentDir, 'memes'), (err) => {
   if (err) {
     return console.error(err);
   }
   console.log('Directory created successfully!');
 });
+
+// Fetch data with Node-fetch and scrape using Cheerio.
+const response = await fetch(
+  'https://memegen-link-examples-upleveled.netlify.app/',
+);
+const body = await response.text();
+const imageUrls = [];
+
+// Loop through Array.
+const $ = cheerio.load(body);
+const myFolder = './memes';
+for (let i = 0; i < 10; i++) {
+  const image = $('img', body)[i].attribs.src;
+  const myUrls = imageUrls.push(image);
+  fetch(image).then((res) => {
+    const dest = fs.createWriteStream(`${myFolder}/memes${i + 1}.jpg`);
+    res.body.pipe(dest);
+  });
+}
